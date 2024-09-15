b286d563cdf8f13a6ab1f64bedce1d5b3576fd53
KRISH826 <krishnendupanj@gmail.com>
2024-09-15 12:27:56 +0530

    Mwember edit structure done

--- next-js-training/src/app/members/[userId]/layout.tsx
+++ next-js-training/src/app/members/[userId]/layout.tsx
@@ -13,11 +13,17 @@ const Layout = async ({
 }) => {
   const member = await getMemberUserById(params.userId);
   if (!member) return notFound();
+  const basePath = `/members/${member.userId}`;
+  const navlinks = [
+    { name: "Profile", href: `${basePath}` },
+    { name: "Photos", href: `${basePath}/photos` },
+    { name: "Chat", href: `${basePath}/chats` },
+  ];
   return (
     <>
       <div className="grid grid-cols-12 gap-5 h-[calc(100vh-115px)]">
         <div className="col-span-3">
-          <MemberSidebar member={member} />
+          <MemberSidebar member={member} navlinks={navlinks} />
         </div>
         <div className="col-span-9">
           <Card className="w-full h-[calc(100vh-115px)]">{children}</Card>
