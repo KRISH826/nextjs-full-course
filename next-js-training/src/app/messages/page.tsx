e8bc9362dd7839f48e546c3da7f818cac8957039
KRISH826 <krishnendupanj@gmail.com>
2024-10-03 17:12:01 +0530

    message sidebar UI done and push

--- next-js-training/src/app/messages/page.tsx
+++ next-js-training/src/app/messages/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import MessageSidebar from './MessageSidebar'
 
 export default function MessagePage() {
   return (
-    <div>
-      MessagePage
+    <div className='grid grid-cols-12 gap-5 h-[calc(100vh-115px)] mt-10'>
+      <div className='col-span-2'>
+        <MessageSidebar />
+      </div>
+      <div className='col-span-10'>
+        <p>text messages</p>
+      </div>
     </div>
   )
 }
